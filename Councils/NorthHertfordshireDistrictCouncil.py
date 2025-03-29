5053708abc6adf527ed75e2e350c3a9d9ed077e3
JackMilner1 <117851346+JackMilner1@users.noreply.github.com>
2025-03-29 22:55:10 +0000

    North Herts Added

--- Councils/NorthHertfordshireDistrictCouncil.py
+++ Councils/NorthHertfordshireDistrictCouncil.py
@@ -2,40 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 
 def getData():
-    thing = "ADDRESS"
-    user_postcode = "SG1 6BH"
-    user_paon = "9"
+    fullAddress = ""
+    user_postcode = ""
+    user_paon = ""
     url = "https://uhtn-wrp.whitespacews.com/"
     session = requests.Session()
     hashedLink = session.get(url)
     hashedLink.raise_for_status()
 
+
+    # Home page 
     doc = BeautifulSoup(hashedLink.text,"html.parser")
     alink = doc.find("a", text="Find my bin collection day")
-    address_field = doc.find("input",{"id":"address_postcode"})
     nextpageurl = alink["href"].replace("seq=1", "seq=2")
-    print(address_field)
-    print("\n\n\n" + alink["href"])
 
-    print(nextpageurl)
+    # Enter user data to parse to next page so correct address data is sent
 
     data = {
         "address_name_number": user_paon,
         "address_postcode": user_postcode,
     }
 
-    link = session.post(nextpageurl,data)
+    # Gets the page with a list of addresses you can select from
 
+    link = session.post(nextpageurl,data)
     link.raise_for_status() 
     doc = BeautifulSoup(link.text,"html.parser")
-    alink = doc.find("a", text=thing)
-    binDataURL = url + alink["href"]
+    alink = doc.find("a", text=fullAddress) # finds the address matching to the users
+    binDataURL = url + alink["href"] # full link for bin data
+
+    # Gets page with bin data
+
     binData = session.get(binDataURL)
     doc = BeautifulSoup(binData.text,"html.parser")
-    #print(doc.prettify())
+    u1s = doc.find("section", id="scheduled-collections").find_all("u1") # all data entries
 
-    u1s = doc.find("section", id="scheduled-collections").find_all("u1")
 
-    print(u1s)
+    returnData = []
+    print("---------------------------------------------------------------------------")
+    print("-------------------------------Your Bin Data-------------------------------")
+    print("---------------------------------------------------------------------------")
+    for element in u1s:
+        DateAndType = element.find_all("li",recursive=False)
+        # DateAndType[0] is an image so is unnecessary 
+        date = DateAndType[1].text.replace("\n","")
+        binType = DateAndType[2].text.replace("\n","")
+        print(f"{binType} is due at {date}")
+        returnData = returnData + [(binType,date)]
+    print("---------------------------------------------------------------------------")
+    print("---------------------------------------------------------------------------")
+    print("---------------------------------------------------------------------------")
+    
+    return returnData # returns an array of (binType,date) pairs
 
 getData()
