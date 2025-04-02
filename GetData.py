22360226781aaea8667d86064a09f631e507c27d
JackMilner1 <117851346+JackMilner1@users.noreply.github.com>
2025-04-02 22:39:02 +0100

    outline

--- GetData.py
+++ GetData.py
@@ -9,5 +9,18 @@ def makeRequest():
     for pairs in fullData:
         print(f"Your {pairs[0]} is due on the {pairs[1]}")
 
+def findChosenCouncil(address,postcode,poan):
+    # this will check what the postcode is and return the council associated with the postcode
+    # this council can then be imported and getData can be ran
+    pass
+
+def searchPostcode():
+    pass
+
+def getDataRecieverImport():
+    pass
+
+def canUserRequest():
+    pass
 
 makeRequest()
